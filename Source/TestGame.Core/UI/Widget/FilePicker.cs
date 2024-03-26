8bda37c489fb90f9d24b424f4c789d83954567cb
ark <divineswordbearer@gmail.com>
2024-03-26 08:37:11 +0800

    breaking; Changed directory structure

---
+++ Source/TestGame.Core/UI/Widget/FilePicker.cs
@@ -0,0 +1,36 @@
+using ImGuiNET;
+
+namespace Raven.Widget
+{
+  public class FilePicker
+  {
+    bool _isOpenFile = false;
+    Action<string> _pickerCallback;
+    public void Open(Action<string> callback)
+    {
+      _pickerCallback = callback;
+    }
+    public void Draw()
+    {
+      if (_isOpenFile)
+      {
+        ImGui.OpenPopup("file-picker-modal");
+        _isOpenFile = false;
+      }
+      var isOpen = true;
+      if (ImGui.BeginPopupModal("file-picker-modal", ref isOpen, ImGuiWindowFlags.NoTitleBar))
+      {
+        var picker = Nez.ImGuiTools.FilePicker.GetFilePicker(this, Path.Combine(Environment.CurrentDirectory, "Content"), ".png|.atlas");
+        picker.DontAllowTraverselBeyondRootFolder = true;
+        if (picker.Draw())
+        {
+          _pickerCallback.Invoke(picker.SelectedFile);
+          _pickerCallback = null;
+          Nez.ImGuiTools.FilePicker.RemoveFilePicker(this);
+        }
+        ImGui.EndPopup();
+      }
+    }
+
+  }
+}
---
+++ Source/TestGame.Core/UI/Widget/NameModal.cs
@@ -0,0 +1,35 @@
+using ImGuiNET;
+
+namespace Raven.Widget
+{ 
+  public class NameModal
+  {
+    bool _isNameModal;
+    Action<string> _nameCallback;
+    public void Open(Action<string> callback)
+    {
+      _nameCallback = callback;
+    }
+    public void Draw()
+    {
+      if (_isNameModal)
+      {
+        ImGui.OpenPopup("name-action-modal");
+        _isNameModal = false;
+      }
+      var open = true;
+      if (ImGui.BeginPopupModal("name-action-modal", ref open, ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.AlwaysAutoResize))
+      {
+        var input = "";
+        ImGui.SetKeyboardFocusHere();
+        if (ImGui.InputText("Name", ref input, 50, ImGuiInputTextFlags.EnterReturnsTrue))
+        {
+          _nameCallback.Invoke(input);
+          ImGui.CloseCurrentPopup();
+          input = "";
+        }
+        ImGui.EndPopup();
+      }
+    } 
+  }
+}
