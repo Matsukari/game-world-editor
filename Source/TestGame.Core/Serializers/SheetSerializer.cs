ab37d6711b52e92b45a9277eb01a5650f021f82a
ark <divineswordbearer@gmail.com>
2024-03-29 15:32:46 +0800

    For now, shit is good

--- Source/TestGame.Core/Serializers/SheetSerializer.cs
+++ Source/TestGame.Core/Serializers/SheetSerializer.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Nez;
+using Raven.Sheet;
 
 namespace Raven.Serializers
 {
@@ -11,27 +12,28 @@ namespace Raven.Serializers
       model._texture =Texture2D.FromStream(Core.GraphicsDevice, File.OpenRead(model.Filename));
       Insist.IsNotNull(model._texture);  
 
-      Console.WriteLine($"Got spritexs, {model.Spritexes.Count}");
       foreach (var spritex in model.Spritexes)
       {
-        Console.WriteLine($"Got, {spritex.Name}");
         spritex._sheet = model;
         foreach (var part in spritex.Parts)
         {
-          Console.WriteLine($"Got, {part.Name}");
-          Console.WriteLine($"Region, {part.SourceSprite.Region}");
-          Console.WriteLine($"{part.IsLocked}");
-          Console.WriteLine($"{part.Color}");
-          Console.WriteLine($"{part.Origin}");
-          Console.WriteLine($"{part.IsVisible}");
-          Console.WriteLine($"{part.SpriteEffects}");
-          Console.WriteLine($"{part.LocalBounds}");
           part.Spritex = spritex;
           part.SourceSprite._sheet = model;
         }
         foreach (var anim in spritex.Animations)
         {
           anim.Target = spritex;
+          foreach (var frame in anim.Frames)
+          {
+            if (frame is SpritexAnimationFrame spritexFrame) 
+            {
+              foreach (var part in spritexFrame.Parts) 
+              {
+                part.Spritex = spritex;
+                part.SourceSprite._sheet = model;
+              }
+            }
+          }
         }
       }
       foreach (var tile in model.TileMap)
